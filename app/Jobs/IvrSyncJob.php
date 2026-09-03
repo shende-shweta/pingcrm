871cd3b2f708bebd0bb18c16bdbe99e33b77c813
shende-shweta <shweta.shende@globallogic.com>
2026-09-03 12:22:05 +0530

    feat: add IvrSyncJob queued job

---
+++ app/Jobs/IvrSyncJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class IvrSyncJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        public readonly string $table,
+        public readonly int $accountId,
+        public readonly array $payload
+    ) {}
+
+    public function handle(): void
+    {
+        Log::info('IvrSyncJob dispatched', [
+            'table' => $this->table,
+            'account_id' => $this->accountId,
+            'payload_keys' => array_keys($this->payload),
+        ]);
+
+        // Actual remote sync target is TBD per design Open Question #1.
+        // This skeleton will be replaced once the sync destination is confirmed.
+    }
+}
